bbdc95f1f6b7c16c51066df2485d27cd7bf42906
Kirsten Lund <kirsten.d.lund@gmail.com>
2020-07-17 16:07:18 -0400

    adding node server

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-prepData('https://api.covid19api.com/summary')
-.then(resData => {
-  prepMapChart('#map', resData).then(handleMapUpdate)
-})
+// let key = '9faecf21-a17b-44b8-9038-2aad4c0a773f';
+
+const express = require('express');
+const app = express();
+const fetch = require('node-fetch');
+
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('public'));
+
+
+
+// prepData('https://api.covid19api.com/summary')
+// .then(resData => {
+//   prepMapChart('#map', resData).then(handleMapUpdate)
+// })
