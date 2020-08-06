191921de1f9fa3f9c5e098c96e09d86fed229091
Kirsten Lund <kirsten.d.lund@gmail.com>
2020-08-05 21:48:50 -0400

    charts are working

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const Datastore = require('nedb');
 const fetch = require('node-fetch');
+require('dotenv').config();
 
 const app = express();
+
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Starting the server at ${port}`));
 app.use(express.static('public'));
@@ -11,38 +13,35 @@ app.use(express.json({ limit: '1mb' }));
 const database = new Datastore('database.db');
 database.loadDatabase();
 
-app.get('/api', (request, response) => {
-  database.find({}, (err, data) => {  // empty braces to find all, callback 
-    if (err) {
-      response.end();
-      return;
-    }
-    response.json(data);
-  }) 
-});
-
-
-
-app.post('/api', (request, response) => {
-  console.log('I got a request!');
-  console.log(request.body);
-  const data = request.body;
-  const timestamp = Date.now();
-  data.timestamp = timestamp;
-  database.insert(data);
-  response.json(data);
-  // {
-  //   status: 'success',
-  //   timestamp: timestamp,
-  //   latitude: data.lat,
-  //   longitude: data.lon
-  // }
-});
-
-
-// <-------------------------------------  Coding Train
+app.get('/')
 
 // prepData('https://api.covid19api.com/summary')
 // .then(resData => {
 //   prepMapChart('#map', resData).then(handleMapUpdate)
-// })git stat
+// });
+
+
+
+// <-------------------------------------  Coding Train
+
+// app.get('/api', (request, response) => {
+//   database.find({}, (err, data) => {  // empty braces to find all, callback 
+//     if (err) {
+//       response.end();
+//       return;
+//     }
+//     response.json(data);
+//   }) 
+// });
+
+// app.post('/api', (request, response) => {
+//   console.log('I got a request!');
+//   console.log(request.body);
+//   const data = request.body;
+//   const timestamp = Date.now();
+//   data.timestamp = timestamp;
+//   database.insert(data);
+//   response.json(data);
+// });
+// <-------------------------------------  Coding Train
+
